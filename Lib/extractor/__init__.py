a24e9416d08293a82c1299b6d10df3278ae08654
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2011-09-06 01:30:29 +0000

    Added support for Type1 (PFA and PFB). LWFN is not supported because of a FontTools issue.
    Frederik wrote parts of this.

--- Lib/extractor/__init__.py
+++ Lib/extractor/__init__.py
@@ -1,6 +1,7 @@
 from exceptions import ExtractorError
 from formats.opentype import isOpenType, extractFontFromOpenType
 from formats.woff import isWOFF, extractFontFromWOFF
+from formats.type1 import isType1, extractFontFromType1
 
 def extractUFO(pathOrFile, destination, doGlyphs=True, doInfo=True, doKerning=True, customFunctions={}):
     if isOpenType(pathOrFile):
@@ -9,6 +10,9 @@ def extractUFO(pathOrFile, destination, doGlyphs=True, doInfo=True, doKerning=Tr
     elif isWOFF(pathOrFile):
         func = extractFontFromWOFF
         format = "WOFF"
+    elif isType1:
+        func = extractFontFromType1
+        format = "Type1"
     else:
         raise ExtractorError("Unknown file format.")
     func(pathOrFile, destination, doGlyphs=doGlyphs, doInfo=doInfo, doKerning=doKerning, customFunctions=customFunctions.get(format, []))
---
+++ Lib/extractor/formats/type1.py
@@ -0,0 +1,128 @@
+from fontTools.t1Lib import T1Font, T1Error
+from fontTools.agl import AGL2UV
+from extractor.tools import RelaxedInfo
+
+# specification: http://partners.adobe.com/public/developer/en/font/T1_SPEC.PDF
+
+# ----------------
+# Public Functions
+# ----------------
+
+def isType1(pathOrFile):
+    try:
+        font = T1Font(pathOrFile)
+        del font
+    except T1Error:
+        return False
+    return True
+
+def extractFontFromType1(pathOrFile, destination, doGlyphs=True, doInfo=True, doKerning=True, customFunctions=[]):
+    source = T1Font(pathOrFile)
+    if doInfo:
+        extractType1Info(source, destination)
+    if doGlyphs:
+        extractType1Glyphs(source, destination)
+    if doKerning:
+        # kerning extraction is not supported yet.
+        # in theory, it could be retried from an AFM.
+        # we need to find the AFM naming rules so that we can sniff for the file.
+        pass
+    for function in customFunctions:
+        function(source, destination)
+
+def extractType1Info(source, destination):
+    info = RelaxedInfo(destination.info)
+    _extractType1FontInfo(source, info)
+    _extractType1Private(source, info)
+
+# ----
+# Info
+# ----
+
+def _extractType1FontInfo(source, info):
+    sourceInfo = source["FontInfo"]
+    # FontName
+    info.postscriptFontName = source["FontName"]
+    # version
+    version = sourceInfo.get("version")
+    if version is not None:
+        # the spec says that version will be a string and no formatting info is given.
+        # so, only move forward if the string can actually be parsed.
+        try:
+            # 1. convert to a float
+            version = float(version)
+            # 2. convert it back to a string
+            version = "%.3f" % version
+            # 3. split.
+            versionMajor, versionMinor = version.split(".")
+            # 4. convert.
+            versionMajor = int(versionMajor)
+            versionMinor = int(versionMinor)
+            # 5. set.
+            info.versionMajor = int(versionMajor)
+            info.versionMinor = int(versionMinor)
+        except ValueError:
+            # couldn't parse. leve the object with the default values.
+            pass
+    # Notice
+    notice = sourceInfo.get("Notice")
+    if notice:
+        info.copyright = notice.decode("mac_roman")
+    # FullName
+    fullName = sourceInfo.get("FullName")
+    if fullName:
+        info.postscriptFullName = fullName.decode("mac_roman")
+    # FamilyName
+    familyName = sourceInfo.get("FamilyName")
+    if familyName:
+        info.familyName = familyName.decode("mac_roman")
+    # Weight
+    postscriptWeightName = sourceInfo.get("Weight")
+    if postscriptWeightName:
+        info.postscriptWeightName = postscriptWeightName.decode("mac_roman")
+    # ItalicAngle
+    info.italicAngle = sourceInfo.get("ItalicAngle")
+    # IsFixedPitch
+    info.postscriptIsFixedPitch = sourceInfo.get("isFixedPitch")
+    # UnderlinePosition/Thickness
+    info.postscriptUnderlinePosition = sourceInfo.get("UnderlinePosition")
+    info.postscriptUnderlineThickness = sourceInfo.get("UnderlineThickness")
+
+def _extractType1Private(source, info):
+    private = source["Private"]
+    # UniqueID
+    info.openTypeNameUniqueID = private.get("UniqueID", None)
+    # BlueValues and OtherBlues
+    info.postscriptBlueValues = private.get("BlueValues", [])
+    info.postscriptOtherBlues = private.get("OtherBlues", [])
+    # FamilyBlues and FamilyOtherBlues
+    info.postscriptFamilyBlues = private.get("FamilyBlues", [])
+    info.postscriptFamilyOtherBlues = private.get("FamilyOtherBlues", [])
+    # BlueScale/Shift/Fuzz
+    info.postscriptBlueScale = private.get("BlueScale", None)
+    info.postscriptBlueShift = private.get("BlueShift", None)
+    info.postscriptBlueFuzz = private.get("BlueFuzz", None)
+    # StemSnapH/V
+    info.postscriptStemSnapH = private.get("StemSnapH", [])
+    info.postscriptStemSnapV = private.get("StemSnapV", [])
+    # ForceBold
+    info.postscriptForceBold = bool(private.get("ForceBold", None))
+
+# --------
+# Outlines
+# --------
+
+def extractType1Glyphs(source, destination):
+    glyphSet = source.getGlyphSet()
+    for glyphName in sorted(glyphSet.keys()):
+        sourceGlyph = glyphSet[glyphName]
+        # make the new glyph
+        destination.newGlyph(glyphName)
+        destinationGlyph = destination[glyphName]
+        # outlines
+        pen = destinationGlyph.getPen()
+        sourceGlyph.draw(pen)
+        # width
+        destinationGlyph.width = sourceGlyph.width
+        # synthesize the unicode value
+        destinationGlyph.unicode = AGL2UV.get(glyphName)
