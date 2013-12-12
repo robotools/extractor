f4a4d622fe554364d06d70aed629c45dc5081d1d
Tal Leming <tal@typesupply.com>
2013-12-12 13:25:08 -0500

    Experimental support for extracting rom VFB using the vfb2ufo commandline tool.

---
+++ Lib/extractor/formats/vfb.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import tempfile
+import subprocess
+from ufo import extractFontFromUFO
+
+_ufo2vfbLocation = "/usr/local/bin/vfb2ufo"
+
+def _setVfb2ufoLocation(location):
+    global _ufo2vfbLocation
+    _ufo2vfbLocation = location
+
+def haveVfb2ufo():
+    return os.path.exists(_ufo2vfbLocation)
+
+# ----------------
+# Public Functions
+# ----------------
+
+def isVFB(pathOrFile):
+    if not isinstance(pathOrFile, basestring):
+        return False
+    if os.path.splitext(pathOrFile)[-1].lower() != ".vfb":
+        return False
+    return True
+
+def extractFontFromVFB(pathOrFile, destination, doGlyphs=True, doInfo=True, doKerning=True, doGroups=True, doFeatures=True, doLib=True, customFunctions=[]):
+    ufoPath = tempfile.mkdtemp(suffix=".ufo")
+    cmds = [_ufo2vfbLocation, pathOrFile, ufoPath]
+    cmds = subprocess.list2cmdline(cmds)
+    popen = subprocess.Popen(cmds, shell=True)
+    popen.wait()
+    try:
+        extractFontFromUFO(ufoPath, destination,
+            doGlyphs=doGlyphs, doInfo=doInfo,
+            doKerning=doKerning, doGroups=doGroups,
+            doFeatures=doFeatures, doLib=doLib,
+            customFunctions=customFunctions
+        )
+    finally:
+        shutil.rmtree(ufoPath)
