67d669c20040f8240842ef01776a318a7466cc0b
Adrien Tétar <adri-from-59@hotmail.fr>
2015-11-02 21:10:17 +0100

    add .gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+
+try:
+    import fontTools
+except:
+    print("*** Warning: extractor requires FontTools, see:")
+    print("    github.com/behdad/fonttools")
+
+
+setup(name="extractor",
+    version="0.1",
+    description="Tools for extracting data from font binaries into UFO objects.",
+    author="Tal Leming",
+    author_email="tal@typesupply.com",
+    url="http://code.typesupply.com",
+    license="MIT",
+    packages=[
+        "extractor",
+        "extractor.formats"
+    ],
+    package_dir={"":"Lib"}
+)
